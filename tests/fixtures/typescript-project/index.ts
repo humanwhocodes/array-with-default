c310b90ab437880f25caa6f6161f2c2237ee6587
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2021-08-12 18:33:35 -0700

    chore: Remove broken type tests

--- tests/fixtures/typescript-project/index.ts
+++
@@ -1,3 +0,0 @@
-import { ArrayWithDefault } from "@humanwhocodes/array-with-default";
-
-new ArrayWithDefault({ default: 1 });
--- tests/types.test.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview Tests that array-with-default.d.ts is valid.
- */
-/* eslint-disable no-console */
-
-import { execSync } from "child_process";
-execSync("cd tests/fixtures/typescript-project && npm i && tsc --showConfig index.ts");
-console.log("array-with-default.d.ts load: success");
