eda6c6526c01be7dc432284dad080bd970ad53d3
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2021-08-12 18:15:06 -0700

    chore: Fix test.types.js

--- tests/types.test.js
+++ tests/types.test.js
@@ -3,6 +3,6 @@
  */
 /* eslint-disable no-console */
 
-const { execSync } = require("child_process");
+import { execSync } from "child_process";
 execSync("cd tests/fixtures/typescript-project && npm i && tsc --showConfig index.ts");
 console.log("array-with-default.d.ts load: success");
